5275d4b11cf456a756f1c8afe40bf6c9f5b3e363
Jonakirke <jhk@northarc.dk>
2021-07-13 11:15:15 +0200

    added two sample doors and added a click handler callback

--- src/game.js
+++ src/game.js
@@ -1,14 +1,18 @@
 import render from "./render";
 
 const game = () => {
-  // eslint-disable-next-line no-unused-vars
   const ui = render();
+
+  const doorClicked = (door) => {
+    // eslint-disable-next-line no-console
+    console.log("The door was clicked", door);
+  };
 
   const init = () => {
     // eslint-disable-next-line no-console
     console.log("initiated game");
 
-    ui.init();
+    ui.init(doorClicked);
   };
 
   return {
--- src/render.js
+++ src/render.js
@@ -1,8 +1,25 @@
 const render = () => {
-  const init = () => {
+  const init = (doorClicked) => {
     document.querySelector("#app").innerHTML = `
 		<h1>Hello from render.js!</h1>
 	`;
+
+    const room = document.querySelector("#room");
+
+    const left = document.createElement("div");
+    left.classList.add("box", "left");
+    left.addEventListener("click", () => {
+      doorClicked("left");
+    });
+
+    const right = document.createElement("div");
+    right.classList.add("box", "right");
+    right.addEventListener("click", () => {
+      doorClicked("right");
+    });
+
+    room.appendChild(left);
+    room.appendChild(right);
   };
 
   return {
